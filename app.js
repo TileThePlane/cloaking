fef4fcafd84af05e4bb413dbd449c5db0e940391
Kyle Vonderwerth <kylesv@live.com>
2020-07-15 17:05:08 -0700

    change view

--- app.js
+++ app.js
@@ -1,7 +1,22 @@
 const express = require('express');
 const app = express();
+const hbs = require('express-handlebars');
 const port = 3000;
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.use(express.static(__dirname + '/public'));
 
-app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
+app.engine( 'hbs', hbs( {
+    extname: 'hbs',
+    defaultLayout: 'main',
+    layoutsDir: __dirname + '/views/layouts/',
+    partialsDir: __dirname + '/views/partials/'
+}));
+app.set('view engine', 'hbs');
+
+app.get('/', (req, res) => {
+    res.render('home');
+});
+
+app.listen(3000, () => {
+    console.log('The web server has started on port localhost:'+port);
+});
